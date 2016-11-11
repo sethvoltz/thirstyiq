700cf1f4f1ab44e96c171d4dd151244c01892c4e
Seth Voltz <seth@designgods.net>
2016-11-11 01:41:57 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Load environment vars
+require('dotenv').config();
+
+// Require libs
+var service = require('./lib');
+
+// Export handlers
+exports.slash = service.slash.handler;
+exports.authorization = service.authorization.handler;
